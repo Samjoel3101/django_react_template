69189de7f4c6636e7cc8b892a40024e83a253773
Samjoel3101 <samjoel3101@gmail.com>
2020-11-25 17:15:46 +0530

    wired signup form to backend

--- Django_React_Project/frontend/src/components/AuthComponents/SignUp.js
+++ Django_React_Project/frontend/src/components/AuthComponents/SignUp.js
@@ -1,19 +1,96 @@
-import React from 'react';
-import {Form} from 'react-bootstrap'; 
+import React, {useState, useEffect} from 'react';
+import {Cookies} from 'react-cookie';
+import {Form, Button, Alert} from 'react-bootstrap';
+import {useHistory} from 'react-router-dom'; 
+
 import useForm from '../Hooks/useForm';
-import formGroup from '../utils/formUtils';
+import {formGroup} from '../utils/formUtils';
 
 function SignUp() {
-    const intialValues = {username:'', email: '', 
-                          password: '', confirmPassword: ''}
-    const [inputValues, inputChange] = useForm(intialValues) 
+    const intialFormValues = {username:'', email: '', 
+                              password: '', confirmPassword: ''}
+
+    const initialErrorValues = {username: null, email: null, 
+                                password1:null, password2:null}
+    
+    const history = useHistory();
+    
+    const [inputValues, inputChange] = useForm(intialFormValues);
+    const [formError, setFormError] = useState(initialErrorValues);
+
+    const onFormSubmit = (e)=> {
+        e.preventDefault();
+        const cookies = new Cookies();
+        var passwordMismatch = false; 
+        const mismatchMessage = 'Passwords do not match';    
+
+        const registerEndpoint = '/api/accounts/register/'; 
+        const data = {'username': inputValues.username, 
+                      'email': inputValues.email, 
+                      'password1': inputValues.password, 
+                      'password2': inputValues.confirmPassword}; 
+        
+        if (data.password1 != data.password2){
+            passwordMismatch = true;
+        }
+        var submissionFailure = false; 
+        
+        const lookUpOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',
+                       'X-CSRFToken': cookies.get('csrftoken')},
+            body: JSON.stringify(data),
+        };
+
+        fetch(registerEndpoint, lookUpOptions).then(
+            response =>{
+                if (response.status === 400){
+                    submissionFailure = true; 
+                }
+                return response.json() 
+            } 
+            ).then(response => {
+                if (submissionFailure){
+                    let errorValues = {username: null, email: null, 
+                        password1:null, password2:null}
+                    for (var key in response){
+                        if (key in errorValues){
+                            errorValues[key] = response[key]
+                        }   
+                    }
+                    if (passwordMismatch === false){
+                        setFormError(errorValues)} 
+                    else{
+                        if (errorValues.password1 !== null){
+                            errorValues.password1.push(mismatchMessage)
+                        }else{
+                            errorValues.password1 = [mismatchMessage]
+                        }
+                        setFormError(errorValues)
+                    }
+                }
+                else{
+                    setFormError(initialErrorValues)
+                   localStorage.setItem('key', response.key)
+                   alert('Logged In Successfully')
+                   history.push('/')
+                }
+            }).catch (errors => {
+                console.log(errors)
+            })
+       
+    };
+    console.log(formError);
     return (
         <div>
-            <Form>
-                {formGroup('text', 'username', 'Username', inputValues.username, inputChange)}
-                {formGroup('email', 'email', 'Email Address', inputValues.email, inputChange)}
-                {formGroup('password', 'password', 'Password', inputValues.password, inputChange)}
-                {formGroup('password', 'confirmPassword', 'Confirm Password', inputValues.confirmPassword, inputChange)}
+            <Form onSubmit = {onFormSubmit}>
+                {formGroup('text', 'username', 'Username', inputValues.username, inputChange, formError.username)}
+                {formGroup('email', 'email', 'Email Address', inputValues.email, inputChange, formError.email)}
+                {formGroup('password', 'password', 'Password', inputValues.password, inputChange, formError.password1)}
+                {formGroup('password', 'confirmPassword', 'Confirm Password', inputValues.confirmPassword, inputChange, formError.password2)}
+                <Button type = 'submit'>
+                    Submit
+                </Button>
             </Form>
         </div>
     )
--- Django_React_Project/frontend/src/components/utils/formUtils.js
+++ Django_React_Project/frontend/src/components/utils/formUtils.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Form} from 'react-bootstrap';
+import {Form, Alert} from 'react-bootstrap';
 
-function formGroup(fieldType, fieldName, labelText, inputValue, onChangeFunc){
+function formAlerts(errors){
+    if (errors !== null){
+    return errors.map((value, index, arr) => {
+        return (<Alert variant = 'danger'>
+            {value}
+        </Alert>)
+    })
+    }else {
+        return null; 
+    };  
+}
+
+function formGroup(fieldType, fieldName, labelText, inputValue, onChangeFunc, error){
+    const alert_components = formAlerts(error)
+
     return (
     <Form.Group controlId={`formBasic${fieldType}`}>
+        {alert_components}
         <Form.Label>{labelText}</Form.Label>
         <Form.Control type={fieldType} placeholder="" name = {fieldName} value = {inputValue} onChange = {onChangeFunc}/>
     </ Form.Group>)
 }
 
-export default formGroup;
+export {formGroup, formAlerts};
