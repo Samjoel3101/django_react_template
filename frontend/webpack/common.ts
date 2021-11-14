199f94c85de83df27e4ba563844062d03a7dc8a9
SamJoel <samjoel3101@gmail.com>
2021-11-14 22:38:24 +0530

    frontend: add common webpack config

---
+++ frontend/webpack/common.ts
@@ -0,0 +1,33 @@
+import path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+};
+
+export default config;
